8bacc0a95e6157ba8d65c28d4c0cde264368e277
Denisuardi <denisuardi05@gmail.com>
2020-11-06 14:34:31 +0700

    membersihkan boiler plate reactjs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Ini aplikasi buku
-        </p>
-        <p>
-          Ini paragraf baru
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Welcome to Suka Buku</h1>
+    </>
   );
 }
 
