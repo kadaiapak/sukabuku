b264f840b24296b6ab8106c9c0b3c3c7cfd87169
Denisuardi <denisuardi05@gmail.com>
2021-01-01 01:20:44 +0700

    menambahkan model pada mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
 const products = require('./data/products');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config()
 
+connectDB()
+
+const app = express();
 app.get('/',(req,res) => {
     res.send ('API Running ....')
 })
